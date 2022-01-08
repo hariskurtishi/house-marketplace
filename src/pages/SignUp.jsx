73172fdc09f56fc15839c2dc48ac38b3a9411e13
hariskurtishi <Hariskay95@gmail.com>
2022-01-08 11:49:56 +0100

    house-marketplace-added-some-changes

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -3,6 +3,7 @@ import {Link, useNavigate} from 'react-router-dom'
 import { toast } from 'react-toastify'
 import {getAuth, createUserWithEmailAndPassword, updateProfile} from 'firebase/auth'
 import { setDoc, doc, serverTimestamp } from 'firebase/firestore'
+import OAuth from '../components/OAuth'
 import { db } from '../firebase.config'
 import {ReactComponent as ArrowRightIcon} from  '../assets/svg/keyboardArrowRightIcon.svg'
 import visibilityIcon from '../assets/svg/visibilityIcon.svg'
@@ -98,7 +99,7 @@ function SignUp() {
                         </div>
                     </form>
 
-                    {/*google oauth*/}
+                  <OAuth />
 
                     <Link to='/sign-in' className='registerLink'>
                         Sign In Instead
